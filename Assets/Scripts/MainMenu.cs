3d44c8f8c15c576279ea5c2e5c76ea498eb0c65f
Adriana <57851464+StormingChaos@users.noreply.github.com>
2023-03-27 22:33:18 -0400

    Main menu skeleton

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // call when start dungeon button is clicked
+    public void StartPrototype()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    // call when start building button is clicked
+    public void StartDemo()
+    {
+        SceneManager.LoadScene("NewMap");
+    }
+
+    // call when quit button is clicked
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
+}
